b6254f6f9aa765a211d7c88456296a1beed6b3ee
Rose Wisotzky <rose@rosewisotzky.com>
2019-05-28 15:14:27 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace idiom_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
